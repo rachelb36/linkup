805f50aa3d3681e419da2b9fc52467379ae1e45b
rburgos <rachel@pixelstreetmedia.com>
2024-11-01 03:36:20 -0500

    modified server.js

--- server/server.js
+++ server/server.js
@@ -10,11 +10,6 @@ const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
-}
-);
-
 
 const server = new ApolloServer({
   typeDefs,
